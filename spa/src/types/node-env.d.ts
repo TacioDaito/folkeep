a0c75c2e634e5da8ea6044fb751c39384470b6f9
TacioDaito <taciokikuchi@gmail.com>
2026-03-13 09:25:58 -0300

    Optim: Optimizing Keycloak startup and project cleaning

--- spa/src/types/node-env.d.ts
+++
@@ -1,7 +0,0 @@
-declare namespace NodeJS {
-    export interface ProcessEnv {
-      NEXT_PUBLIC_AUTHENTIK_CLIENT_ID: string
-      NEXT_PUBLIC_AUTHENTIK_CLIENT_SECRET: string
-      NEXT_PUBLIC_AUTHENTIK_URL: string
-    }
-  }
