a80fbca86546016ae14ed0ac84f0ce5d163835a4
TacioDaito <taciokikuchi@gmail.com>
2026-04-07 20:47:26 -0300

    Add cache to user service

--- api/app/Services/UserService.php
+++ api/app/Services/UserService.php
@@ -5,6 +5,7 @@ namespace App\Services;
 use Illuminate\Database\Eloquent\Model;
 use App\Contracts\UserServiceInterface;
 use App\Models\User;
+use Illuminate\Support\Facades\Cache;
 
 /**
  * UserService is responsible for handling user-related business logic.
@@ -16,6 +17,8 @@ class UserService implements UserServiceInterface
 {
     public function getUser(string $sub): Model
     {
-        return User::where('keycloak_id', $sub)->firstOrFail();
+        return Cache::remember("user:{$sub}", now()->addMinutes(10), function () use ($sub) {
+            return User::where('keycloak_id', $sub)->select(['id', 'name', 'email', 'keycloak_id'])->firstOrFail();
+        });
     }
 }
