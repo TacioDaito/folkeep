2e7a5e09ed847e4cecfe7d743379c11aac318fd3
TacioDaito <taciokikuchi@gmail.com>
2026-04-13 00:26:13 -0300

    test

--- api/routes/api.php
+++ api/routes/api.php
@@ -5,5 +5,5 @@ use App\Http\Controllers\UserController;
 
 Route::middleware('keycloak.auth')->group(function () {
     Route::get('/hello', fn() => response()->json(['message' => 'Hello!']));
-    Route::get('/user', [UserController::class, 'getUser']);
+    Route::get('/user', [UserController::class, 'getUser'])->name('user.info');
 });
