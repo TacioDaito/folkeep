b31f7a4d8114332c024e23b14c2512a7c111f094
TacioDaito <taciokikuchi@gmail.com>
2026-05-26 15:39:44 -0300

    refactor: consolidate keycloak config and introduce EnsureUserExists middleware
    Simplify Keycloak configuration by removing obsolete env variables and
    directly using KEYCLOAK_JWKS_URL and KEYCLOAK_ISSUER. Add
    EnsureUserExists middleware to create or update users after token
    validation, and adjust User model fillables to include keycloak_id.

--- api/config/keycloak.php
+++ api/config/keycloak.php
@@ -2,31 +2,11 @@
 
 return [
 
-    // The base URL of the Keycloak server
-    'base_url'       => env('KEYCLOAK_BASE_URL', 'http://localhost:8080'),
-
-    // The realm name in Keycloak that this application belongs to
-    'realm'          => env('KEYCLOAK_REALM', 'master'),
-
-    // The client ID registered in Keycloak for this application
-    'client_id'      => env('KEYCLOAK_CLIENT_ID', 'my-api'),
-
     // The JWKS URI for fetching public keys to validate JWT tokens
-    'jwks_uri'       => env(
-        'KEYCLOAK_JWKS_URI',
-        env('KEYCLOAK_BASE_URL', 'http://localhost:8080')
-        . '/realms/'
-        . env('KEYCLOAK_REALM', 'master')
-        . '/protocol/openid-connect/certs'
-    ),
+    'jwks_uri'       => env('KEYCLOAK_JWKS_URL'),
 
     // The issuer URL for validating the "iss" claim in JWT tokens
-    'issuer'         => env(
-        'KEYCLOAK_ISSUER',
-        env('KEYCLOAK_PUBLIC_URL', 'http://localhost:8080')
-        . '/realms/'
-        . env('KEYCLOAK_REALM', 'master')
-    ),
+    'issuer'         => env('KEYCLOAK_ISSUER'),
 
     // Cache TTL for JWKS keys in seconds (default: 3600 seconds = 1 hour)
     'jwks_cache_ttl' => env('KEYCLOAK_JWKS_CACHE_TTL', 3600),
